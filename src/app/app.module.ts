746d78ba75ca853d8a739ea41ba9d938e2bc2586
metiago <tiagotg.ribeirogmail.com>
2019-05-16 19:13:09 +0100

    Added http and form modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ControlMessagesComponent } from "./control-messages.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlMessagesComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
