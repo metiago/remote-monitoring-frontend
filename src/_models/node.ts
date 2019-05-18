9c620da1bafac331c328df378a3b73ea57e04670
metiago <tiagotg.ribeirogmail.com>
2019-05-18 18:01:24 +0100

    Implemented web socket monitoring

--- src/_models/node.ts
+++ src/_models/node.ts
@@ -1,8 +1,10 @@
 ﻿export class Node {
+    key: string;
     pollFrequency: number;
     start: string;
     end: string;
     expire: number;
     host: string;
     port: number;
+    up: boolean;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { MonitorService } from "../_services/monitor.service";
 import { MaterialHelper } from "../_services/material";
 import { Node } from 'src/_models';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,8 +14,8 @@ export class AppComponent implements OnInit {
 
   title = 'Monitoring';
   mNodeForm = "#mNodeForm"
-  nodes: Node[];  
-  nodeForm: FormGroup;
+  nodes: Node[]
+  nodeForm: FormGroup
 
   constructor(private formBuilder: FormBuilder,
     private materialHelper: MaterialHelper,
@@ -34,6 +33,28 @@ export class AppComponent implements OnInit {
     });
 
     this.getAll()
+        
+    var ws = new WebSocket("ws://localhost:8001");
+    
+    ws.onopen = function (event) {
+      console.log("socket open");
+    };
+
+    const that = this
+    ws.onmessage = function (event) {
+      const blb = new Blob([event.data], { type: "text/plain" });
+      const reader = new FileReader();
+
+      reader.addEventListener('loadend', (e) => {
+        
+        const node: Node = JSON.parse(e.srcElement.result);
+        
+        that.setStatus(node)
+
+      });
+
+      reader.readAsText(blb);
+    }
   }
 
   openModal() {
@@ -53,7 +74,7 @@ export class AppComponent implements OnInit {
   }
 
   detail(key: string) {
-    this.monitorService.detail(key).subscribe((node) => {      
+    this.monitorService.detail(key).subscribe((node) => {
       this.materialHelper.openModal(this.mNodeForm)
       this.materialHelper.updateFields()
       this.nodeForm.patchValue(node);
@@ -69,6 +90,15 @@ export class AppComponent implements OnInit {
     }
   }
 
+  setStatus(node: Node) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      const elem = this.nodes[i]          
+      if (elem.key === node.key) {            
+        this.nodes.splice(i, 1, node);              
+      }          
+    }
+  }
+  
   onSubmit() {
 
     if (this.nodeForm.invalid) {
